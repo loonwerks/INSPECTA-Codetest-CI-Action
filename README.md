325be281f6b52396e99bfc44df2b15501be24c52
Karl Hoech <karl.hoech@collins.com>
2026-02-11 10:37:47 -0600

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # INSPECTA-Codetest-CI-Action
+
 Github action to conduct GUMBOX tests
+
+## Inputs
+
+### `sourcepath`
+
+Path to top level Makefile (expects path string).
+
+### `environment-variables`
+
+JSON-formatted dictionary of environment variables to pass to the make system.
+
+## Outputs
+
+## `result`
+
+The JSON-formatted summary of analysis results.
+
+## Example usage
+
+uses: actions/INSPECTA-Codetest-CI-Action@v1
+with:
+  sourcepath: 'system/hamr/microkit'
---
+++ entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+echo "sourcepath: $1"
+echo "environment-variables: $2"
+
+rustup toolchain list
+rustup target list | grep \(installed\)
+rustup component list | grep \(installed\)
+
+sourcePath=system/hamr/microkit
+if [[ -n $1 ]]; then
+	sourcePath=$1
+fi
+
+if [[ -n $2 ]]; then
+	for kv in $(echo $2 | jq -r 'to_entries | .[] | .key + "=" + (.value | @sh)'); do
+		echo "setting ${kv}"
+		export $kv;
+	done
+fi
+
+runCommand=(make -C $GITHUB_WORKSPACE/${sourcePath} test)
+
+outputFile="codetest.out"
+if [[ -n $3 ]]; then
+	outputFile=$3
+fi
+
+echo "run command: ${runCommand[@]}" 
+
+"${runCommand[@]}" >> "$outputFile"
+EXIT_CODE=$?
+
+echo "timestamp=$(date)" >> $GITHUB_OUTPUT
+echo "status=${EXIT_CODE}" >> $GITHUB_OUTPUT
+echo "status-messages=$(cat ${outputFile} | jq -R -s '.')" >> $GITHUB_OUTPUT
+
+echo "exit code: $EXIT_CODE"
+if [ "XX $EXIT_CODE" = "XX 0" ]; then
+	exit 0
+else
+	exit 1
+fi
